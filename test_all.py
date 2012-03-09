4dc560ca875e7152c9279e468d37f8da88d9b704
Shuhao <admin@thekks.net>
2012-03-09 11:46:44 -0500

    Fixed bugs and moved docstrings
    Documentation for HttpTransport has been moved to Transport as all
    implemented subclass should take the same input and output.
    
    Additional test cases has been added.

--- test_all.py
+++ test_all.py
@@ -63,10 +63,40 @@ class Riak2CoreHttpTransportTest(unittest.TestCase):
         self.transport.delete("test_bucket", "foo", 2)
         self.transport.delete("test_bucket", "bar", 2)
 
+    def test_put_no_key(self):
+        headers = self.transport.make_put_header("application/json", [], [], {})
+        result = self.transport.put("test_bucket", None, "{1 : 2}", headers, 2, 2)
+        self.assertEqual(3, len(result))
+        key, vclock, metadata = result
+        self.assertEqual(201, metadata["http_code"])
+        result = self.transport.get("test_bucket", key, 2)
+        self.assertEqual(3, len(result))
+        self.assertEqual(200, result[1]["http_code"])
+        self.assertEqual("{1 : 2}", result[2])
+        self.transport.delete("test_bucket", key, 2)
+
     def test_delete404(self):
         # Should not raise an error
         self.transport.delete("test_bucket", "foo", 2)
 
+    def test_get_keys(self):
+        # This needs {delete_mode, immediate} to pass.
+        # Or else, foo will "leak" into this.
+
+        result = self.transport.get_keys("test_bucket")
+        self.assertEqual(0, len(result))
+
+        headers = self.transport.make_put_header("application/json", [], [], {})
+        self.transport.put("test_bucket", "foo", "test", headers, 2, 2)
+        self.transport.put("test_bucket", "bar", "test", headers, 2, 2)
+        result = self.transport.get_keys("test_bucket")
+        self.assertEqual(2, len(result))
+        self.transport.delete("test_bucket", "foo", 2)
+        self.transport.delete("test_bucket", "bar", 2)
+        self.assertEqual({u"bar", u"foo"}, set(result)) # Convert to set for comparison
+
+        result = self.transport.get_keys("test_bucket")
+        self.assertEqual(0, len(result))
 
 
 
